06bde836b379f49f0144f4c7ad1fb4629c547472
laraameg <92958032+laraameg@users.noreply.github.com>
2022-03-12 12:59:51 -0300

    Update Quiz.js

--- js/Quiz.js
+++ js/Quiz.js
@@ -49,19 +49,19 @@ class Quiz {
         debugger;
         var correctAns = "2";
 
-        // if (correctAns !== allContestants[plr].answer){
-        //   fill("Green")
-        // }
-        // else{
-        //   fill("red");
-        // }
-
-         if (correctAns === allContestants[plr].answer){
+         if (correctAns !== allContestants[plr].answer){
            fill("Green")
          }
          else{
            fill("red");
          }
+
+         //if (correctAns === allContestants[plr].answer){
+         //  fill("Green")
+         //}
+        // else{
+         //  fill("red");
+         //}
 
         // if (correctAns === allContestants[plr].answer){
         //    fill("red")
